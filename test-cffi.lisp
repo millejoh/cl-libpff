6af5e200b5c57ae95e278344de5698b05f124dd8
John Miller <millejoh@mac.com>
2022-09-05 20:05:59 -0500

    File basics.
    Can create and open files, but having a hard time getting pass by
    reference calls to work.

--- test-cffi.lisp
+++ test-cffi.lisp
@@ -3,13 +3,15 @@
 
 (in-package :cl-libpff-test-cffi)
 
-(defparameter *test-pst-file* "/mnt/c/Users/mille/Documents/Outlook Files/Outlook1.pst")
+(defparameter *test-pst-file-1* "/mnt/c/Users/mille_9b1cm14/OneDrive/Documents/Outlook Files/test.pst")
+(defparameter *test-pst-file-2* "/mnt/c/Users/mille_9b1cm14/AppData/Local/Microsoft/Outlook/johnpantagruel@gmail.com.ost")
 
 (define-foreign-library libpff
   (:unix (:or "libpff.so" "libpff.so.1" "/usr/local/lib/libpff.so"))
   (t (:default "libpff")))
 
 (use-foreign-library libpff)
+
 
 (defcfun "libpff_get_version" :string)
 
@@ -44,7 +46,7 @@
     (unless (= 1 (libpff-file-initialize f (null-pointer)))
       (warn "Something went wrong initializing a file object."))
     (unless (null-pointer-p f)
-      (libpff-file-free f (null-pointer)))
+      (libpff-file-f))
     (foreign-free f)))
 
 (defcfun "libpff_file_open" :int
@@ -57,7 +59,37 @@
   (file :pointer)
   (error :pointer))
 
-(defun test-file-open (&optional (path *test-pst-file*))
+(defcfun "libpff_file_get_size" :int
+  (file :pointer)
+  (size (:pointer :int64))
+  (err :pointer))
+
+(defcfun "libpff_file_get_content_type" :int
+  (file :pointer)
+  (content-type (:pointer :uint8))
+  (err :pointer))
+
+(defcfun "libpff_file_get_type" :int
+  (file :pointer)
+  (type (:pointer :uint8))
+  (err :pointer))
+
+(defcfun "libpff_file_get_message_store" :int
+  (file :pointer)
+  (message-store :pointer)
+  (err :pointer))
+
+(defcfun "libpff_file_get_root_item" :int
+  (handle :pointer)
+  (root-item :pointer)
+  (err :pointer))
+
+(defcfun "libpff_file_get_root_folder" :int
+  (handle :pointer)
+  (root-folder :pointer)
+  (err :pointer))
+
+(defun test-file-open (&optional (path *test-pst-file-1*))
   (unless (probe-file path)
     (warn "File ~A does not exist.~%" path))
   (let ((f (foreign-alloc :pointer :initial-element (null-pointer))))
@@ -71,3 +103,64 @@
       (warn "Something went wrong closing file ~A.~%" path))
     (libpff-file-free f (null-pointer))
     (foreign-free f)))
+
+(defparameter +default-error+ (cffi:null-pointer))
+
+(defun make-t-pointer ()
+  (cffi:foreign-alloc :pointer :initial-element (cffi:null-pointer)))
+
+(defclass pst-object ()
+  ((handle :accessor pst-handle :initform (make-t-pointer))))
+
+(defclass pst-store (pst-object)
+  ((source :accessor pst-store-source :initarg :source)
+   (type :reader pst-store-type :initform 0)
+   (root-folder :reader pst-store-root-folder :initform (make-t-pointer))
+   (message-store :reader pst-store-message-store :initform (make-t-pointer))
+   (size :reader pst-store-size :initform 0)))
+
+(defclass item (pst-object)
+  ((id :reader pst-item-id :initform 0)
+   (subitem-count :reader pst-)))
+
+(defmacro define-libpff-accessor (object-type accessor return-type)
+  (let ((ffi-accessor-fn (intern (format nil "LIBPFF-~A-GET-~A" object-type accessor)))
+        (accessor-fn (intern (format nil "_GET-~S-~S" object-type accessor))))
+    `(defun ,accessor-fn (ptr)
+       (with-foreign-object (rval ,return-type)
+         (let ((result (,ffi-accessor-fn ptr rval +default-error+)))
+           (values (mem-ref rval ,return-type) result))))))
+
+(define-libpff-accessor file size :long)
+(define-libpff-accessor file type :uint8)
+(define-libpff-accessor file content-type :uint8)
+;; (define-libpff-accessor item identifier :unsigned-int)
+;; (define-libpff-accessor item number-of-entries :unsigned-int)
+;; (define-libpff-accessor item number-of-sub-items :unsigned-int)
+;; (define-libpff-accessor item type :unsigned-char)
+;; (define-libpff-accessor folder type ::unsigned-char)
+;; (define-libpff-accessor folder utf8-name-size :int)
+;; (define-libpff-accessor folder utf16-name-size :int)
+;; (define-libpff-accessor folder number-of-sub-folders :int)
+
+(defmethod initialize-instance :after ((store pst-store) &key &allow-other-keys)
+  (with-slots (handle source type size root-folder message-store) store
+    (format t  "~A" source)
+    (libpff-file-initialize handle (cffi:null-pointer))
+    (when (probe-file source)
+      (let ((rtype (foreign-alloc :uint8 :initial-element 0))
+            (rsize (foreign-alloc :int :initial-element 0)))
+        (when (libpff-file-open handle source 1 +default-error+)
+          (libpff-file-get-content-type handle rtype +default-error+)
+          (libpff-file-get-size handle rsize +default-error+)
+          (libpff-file-get-root-folder handle root-folder +default-error+)
+          (libpff-file-get-message-store handle message-store +default-error+)
+          (setf type (mem-aref rtype :uint8)
+                size (mem-aref rsize :int))
+          (foreign-free rtype)
+          (foreign-free rsize))))))
+
+(defun new-test-store (&optional (filename *test-pst-file-1*))
+  (unless (probe-file filename)
+    (error "File does not exist: ~A." filename))
+  (make-instance 'pst-store :source *test-pst-file-1*))
--- test.lisp
+++ test.lisp
@@ -3,13 +3,15 @@
 
 (in-package :cl-libpff-test)
 
-(defparameter *test-pst-file* "/mnt/c/Users/mille/Documents/Outlook Files/Outlook1.pst")
+(defparameter *test-pst-file-1* "/mnt/c/Users/mille_9b1cm14/OneDrive/Documents/Outlook Files/test.pst")
+(defparameter *test-pst-file-2* "/mnt/c/Users/mille_9b1cm14/AppData/Local/Microsoft/Outlook/johnpantagruel@gmail.com.ost")
 
 (defun test-version ()
   (libpff-get-version))
 
 (defun test-file-signature ()
-  (let ((fname "/mnt/c/Users/mille/Documents/Outlook Files/Outlook1.pst")
+  (let ((fname "/mnt/c/Users/mille/Documents/Outlook Files/Outlook1.pst"
+               )
         (invalid-fname "/home/millejoh/Dropbox/20170627 Scarlett DS11_Complete.pdf"))
     (if (= 1 (libpff-check-file-signature fname (cffi:null-pointer)))
         (format t "~A is a valid PFF datafile.~%" fname)
@@ -19,22 +21,92 @@
         (warn "Something isn't right with signature of ~A.~%" fname))))
 
 (defun test-initialize-file ()
-  (let ((f (cffi:null-pointer)))
-    (print (libpff-file-initialize f (cffi:null-pointer)))
-    (print (libpff-file-free f (cffi:null-pointer)))))
+  (let ((f (cffi:foreign-alloc :pointer :initial-element (cffi:null-pointer))))
+    (unless (= 1 (libpff-file-initialize f (cffi:null-pointer)))
+      (warn "Something went wrong initializing a file object."))
+    (unless (cffi:null-pointer-p f)
+      (libpff-file-free f (cffi:null-pointer)))
+    (cffi:foreign-free f)))
 
-(defun test-open-pst ()
-  (c-let ((f libpff-ffi::libpff-file-t :value (cffi:null-pointer))
-          (e libpff-ffi::libpff-error-t)
-          (size :int))
-    (print (libpff-ffi::libpff-file-initialize (f &) (e &)))
-    (libpff-ffi::libpff-error-free (e &))
-    (print (libpff-ffi::libpff-file-open (f &) *test-pst-file* 0 (e &)))
-    (libpff-ffi::libpff-error-free (e &))
-    (print (libpff-ffi::libpff-file-get-size (f &) (size &) (e &)))
-    (libpff-ffi::libpff-error-free (e &))
-    (print size)
-    (print (libpff-ffi::libpff-file-close (f &) (e &)))
-    (libpff-ffi::libpff-error-free (e &))
-    (print (libpff-ffi::libpff-file-free (f &) (e &)))
-    (libpff-ffi::libpff-error-free (e &))))
+(defun make-file-handle ()
+  (let ((f (cffi:foreign-alloc :pointer :initial-element (cffi:null-pointer))))
+    (unless (= 1 (libpff-file-initialize f (cffi:null-pointer)))
+      (warn "Something went wrong initializing a file object."))
+    f))
+
+(defun free-file-handle (handle)
+  (unless (cffi:null-pointer-p handle)
+    (libpff-file-free handle (cffi:null-pointer))))
+
+(defun test-open-pst (&optional (file-name *test-pst-file-1*))
+  (let* ((f (make-file-handle))
+         (errptr (cffi:null-pointer)))
+    (c-let ((handle :pointer :ptr f)
+            (size :int))
+      (print (libpff-ffi::libpff-file-open handle file-name 1 errptr))
+      (print (libpff-ffi::libpff-file-get-size handle (size &) errptr))
+      (print size)
+      (free-file-handle f))))
+
+(defparameter +default-error+ (cffi:null-pointer))
+
+(defun make-t-pointer ()
+  (cffi:foreign-alloc :pointer :initial-element (cffi:null-pointer)))
+
+(defclass pst-object ()
+  ((handle :accessor pst-handle :initform (make-t-pointer))))
+
+(defclass pst-store (pst-object)
+  ((source :accessor pst-store-source :initarg :source)
+   (type :reader pst-store-type :initform 0)
+   (root-folder :reader pst-store-root-folder :initform (make-t-pointer))
+   (message-store :reader pst-store-message-store :initform (make-t-pointer))
+   (size :reader pst-store-size :initform 0)))
+
+(defclass item (pst-object)
+  ((id :reader pst-item-id :initform 0)
+   (subitem-count :reader pst-)))
+
+
+(defmacro define-libpff-accessor (object-type accessor return-type)
+  (let ((ffi-accessor-fn (intern (format nil "LIBPFF-~A-GET-~A" object-type accessor)))
+        (accessor-fn (intern (format nil "_GET-~S-~S" object-type accessor))))
+    `(defun ,accessor-fn (ptr)
+       (c-let ((rval ,return-type))
+
+         (let ((result (,ffi-accessor-fn ptr (rval &) +default-error+)))
+
+           (values (rval *) result))))))
+
+(define-libpff-accessor item identifier :unsigned-int)
+(define-libpff-accessor item number-of-entries :unsigned-int)
+(define-libpff-accessor item number-of-sub-items :unsigned-int)
+(define-libpff-accessor item type :unsigned-char)
+(define-libpff-accessor file size :int)
+(define-libpff-accessor file type :unsigned-char)
+(define-libpff-accessor file content-type :unsigned-char)
+(define-libpff-accessor folder type ::unsigned-char)
+(define-libpff-accessor folder utf8-name-size :int)
+(define-libpff-accessor folder utf16-name-size :int)
+(define-libpff-accessor folder number-of-sub-folders :int)
+
+(defmethod initialize-instance :after ((store pst-store) &key &allow-other-keys)
+  (with-slots (handle source type size root-folder message-store) store
+    (format t  "~A" source)
+    (libpff-file-initialize handle (cffi:null-pointer))
+    (when (probe-file source)
+      (c-let ((handle :pointer :ptr handle)
+              (rtype :unsigned-char)
+              (rsize :int))
+        (when (libpff-file-open handle source 1 +default-error+)
+          (libpff-file-get-content-type handle (rtype &) +default-error+)
+          (libpff-file-get-size handle (rsize &) +default-error+)
+          (libpff-file-get-root-folder handle root-folder +default-error+)
+          (libpff-file-get-message-store handle message-store +default-error+)
+          (setf type rtype
+                size rsize))))))
+
+(defun new-test-store (&optional (filename *test-pst-file-1*))
+  (unless (probe-file filename)
+    (error "File does not exist: ~A." filename))
+  (make-instance 'pst-store :source *test-pst-file-1*))
